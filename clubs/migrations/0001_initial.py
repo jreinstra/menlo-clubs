4818b8203976139ddf66122e08ac48fa8e8aaa13
yohanstra <jreinstra@gmail.com>
2015-08-22 09:14:01 -0700

    Reset migrations

--- clubs/migrations/0001_initial.py
+++ clubs/migrations/0001_initial.py
@@ -15,7 +15,12 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=50)),
-                ('description', models.CharField(max_length=1000)),
+                ('short_description', models.CharField(max_length=100)),
+                ('description', models.TextField(max_length=1000)),
+                ('meeting_day', models.IntegerField(default=(0, b'Monday'), choices=[(0, b'Monday'), (1, b'Tuesday'), (2, b'Wednesday'), (3, b'Thursday'), (4, b'Friday'), (5, b'Saturday'), (6, b'Sunday')])),
+                ('meeting_time', models.TimeField()),
+                ('meeting_location', models.CharField(max_length=50)),
+                ('contact_email', models.CharField(max_length=200)),
             ],
         ),
     ]
