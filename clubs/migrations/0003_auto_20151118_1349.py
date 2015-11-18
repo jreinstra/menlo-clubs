e087cfb2f1e4096fa0e76c93e86b901e81e52ea0
yohanstra <jreinstra@gmail.com>
2015-11-18 13:57:56 -0800

    test

--- clubs/migrations/0003_auto_20151118_1349.py
+++ clubs/migrations/0003_auto_20151118_1349.py
@@ -14,21 +14,21 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='club',
             name='meets_biweekly',
-            field=models.BooleanField(default=False)
+            field=models.BooleanField(default=False),
         ),
         migrations.AddField(
             model_name='club',
             name='meets_irregularly',
-            field=models.BooleanField(default=False)
+            field=models.BooleanField(default=False),
         ),
         migrations.AddField(
             model_name='club',
             name='meets_monthly',
-            field=models.BooleanField(default=False)
+            field=models.BooleanField(default=False),
         ),
         migrations.AlterField(
             model_name='club',
             name='meeting_day',
-            field=models.IntegerField(default=(0, b'Monday'), choices=[(0, b'Monday'), (1, b'Tuesday'), (2, b'Wednesday'), (3, b'Thursday'), (4, b'Friday'), (5, b'Saturday'), (6, b'Sunday'), (7, b'Non Weekly')])
+            field=models.IntegerField(default=(0, b'Monday'), choices=[(0, b'Monday'), (1, b'Tuesday'), (2, b'Wednesday'), (3, b'Thursday'), (4, b'Friday'), (5, b'Saturday'), (6, b'Sunday'), (7, b'Non Weekly')]),
         ),
     ]
